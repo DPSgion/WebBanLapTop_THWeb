c7bacdb01c82006f5db7cd4d3e351148c3dbc16a
Phuong <np384874@gmail.com>
2025-12-02 16:56:26 +0700

    Tạo cấu trúc trang admin

---
+++ project/admin/index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="vi">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="../assets/css/style_admin.css">
+
+    <title>Admin Page</title>
+</head>
+
+<body>
+    <div class="header">
+        <div class="header-left">
+            <img src="../assets/images/logolaptop.png" width="120px">
+            <h1 class="header-logo">Admin</h1>
+        </div>
+        <div class="header-right">
+            <p class="admin-name">Admin Name</p>
+            <a href="#" class="logout-btn">Đăng xuất</a>
+        </div>
+    </div>
+
+    <div class="sidebar">
+        <ul>
+            <li><a href="">Dashboard</a></li>
+            <li><a href="">Quản lý sản phẩm</a></li>
+            <li><a href="">Quản lý đơn hàng</a></li>
+            <li><a href="">Cài đặt</a></li>
+        </ul>
+    </div>
+
+    <div class="content" id="content">
+        
+    </div>
+
+</body>
+
+</html>
