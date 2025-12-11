dcf3d5e2569626652b2d88c8c8a4f943e3204ee9
Phuong <np384874@gmail.com>
2025-12-11 22:14:21 +0700

    Thêm giao diện con: thêm danh mục cho 'quản lý danh mục' ở trang admin

--- project/admin/quanlydanhmuc.php
+++ project/admin/quanlydanhmuc.php
@@ -1,3 +1,5 @@
+
+
 <div class="quanlydanhmuc">
 
     <div class="main-title">
@@ -9,7 +11,7 @@
             <input type="text" id="search-input" placeholder="Tìm kiếm danh mục ...">
             <button class="btn-search">Tìm</button>
         </div>
-        <a href="?page=category_add" class="btn btn-primary">Thêm danh mục mới</a>
+        <a  id="openModalBtn" class="btn btn-primary">Thêm danh mục mới</a>
     </div>
 
     <div class="table-container">
@@ -54,5 +56,50 @@
         </table>
     </div>
 
+    
+    <div id="myModal" class="modal">
+        <div class="modal-content">
+            <span class="close">&times;</span> <h3>Thêm Danh Mục Mới</h3>
+            
+            <form action="" method="post">
+                <div class="form-group">
+                    <label>Tên danh mục:</label>
+                    <input type="text" name="username" placeholder="Nhập tên danh mục..." required>
+                </div>
+                
+                <div class="form-actions">
+                    <input type="submit" value="Lưu lại" class="btn btn-primary">
+                </div>
+            </form>
+        </div>
+    </div>
+
+
 </div>
 
+
+<script>
+    var modal = document.getElementById("myModal");
+    var btnOpen = document.getElementById("openModalBtn");
+    var btnClose = document.getElementsByClassName("close")[0];
+
+    btnOpen.onclick = function() {
+        modal.style.display = "block";
+    }
+
+    btnClose.onclick = function() {
+        modal.style.display = "none";
+    }
+
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+
+    document.addEventListener('keydown', function(event) {
+        if (event.key === "Escape") {
+            modal.style.display = "none";
+        }
+    });
+</script>
