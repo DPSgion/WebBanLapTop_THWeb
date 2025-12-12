360baf38cf8efa477f9f711cc08bbc5977156f90
Hoang <hoangnguyenminh123321@gmail.com>
2025-12-12 08:25:40 +0700

    thêm header giao diện chinh

---
+++ project/includes/header.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../assets/css/style.css">
+</head>
+<body>
+    <div class="header">
+        <div class="container">
+            <section class="header-bar">
+                <ul>
+                    <li><img src="./assets/images/logolaptop.png" alt="logo-header"></li>
+                    <li><input type="search" class="header-bar-search"placeholder="Nhập tên điện thoại, máy tính..."></li>
+                    <li><img src="./assets/images/user.png" alt="user-header" ></li>
+                    <li><img src="./assets/images/shopping-cart.png" alt=""></li>
+                </ul>
+            </section>
+        </div>
+    </div>
+</body>
+</html>
